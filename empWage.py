e75985b56dd1f58e5ba05111b0896adc103d317a
santospanda111 <santospanda111@gmail.com>
2021-06-15 10:09:28 +0530

    Added UC2 to calculate Salary

--- empWage.py
+++ empWage.py
@@ -1,9 +1,15 @@
 import random
 
 print("Welcome to Employee Wage Computation Program")
-# This condition will check the employee is present or absent.
+empRatePerHr = 20
+empHrs = 8
+'''This condition will check the employee is present or absent.
+   According to the presence of employee it'll calculate the salary. 
+'''
 empOption = random.randint(0, 2)
 if empOption == 1:
     print("Employee is Present")
+    print(f"Employee's Salary is Rs {empHrs*empRatePerHr}/-")
 else:
     print("Employee is Absent")
+    print("Employee's Salary is Rs 0/-")
