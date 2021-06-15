7ea23ac8c80a4c8e212d58606999c059e5a15d0e
santospanda111 <santospanda111@gmail.com>
2021-06-15 09:59:58 +0530

    Added UC1 to check employee is present or absent

--- empWage.py
+++ empWage.py
@@ -1,1 +1,9 @@
+import random
+
 print("Welcome to Employee Wage Computation Program")
+# This condition will check the employee is present or absent.
+empOption = random.randint(0, 2)
+if empOption == 1:
+    print("Employee is Present")
+else:
+    print("Employee is Absent")
