e47b3bc4001fb0edb18355998d4e4dabe56c74a4
santospanda111 <santospanda111@gmail.com>
2021-06-15 11:40:38 +0530

    Added UC4 using switch case

--- empWage.py
+++ empWage.py
@@ -5,17 +5,31 @@ empRatePerHr = 20
 empHrs = 8
 partTimeHr = 4
 
-'''This condition will check the employee is present or absent.
-   According to the presence of employee it'll calculate the salary.
-   Added Part time work. 
 '''
-empOption = random.randint(0, 3)
-if empOption == 1:
-    print("Employee is Present")
-    print(f"Employee's Salary is Rs {empHrs*empRatePerHr}/-")
-elif empOption == 2:
-    print("Employee is present for Half-Time.")
-    print(f"Employee's Salary is Rs {empRatePerHr*partTimeHr}/-")
-else:
-    print("Employee is Absent")
-    print("Employee's Salary is Rs 0/-")
+Added a function which will get the employee hrs.
+according to that it'll print that employee is present or absent. 
+'''
+empOption = random.randint(0, 2)
+
+
+def workingHrs(empOption):
+    switcher = {
+        0: 0,
+        1: empHrs,
+        2: partTimeHr
+    }
+    working_Hr = switcher.get(empOption)
+    if working_Hr == 8:
+        print("Employee is Present")
+    elif working_Hr == 4:
+        print("Employee is Present for Half-Time")
+    else:
+        print("Employee is Absent")
+    return working_Hr
+
+'''
+this method will calculate the salary and return that.
+'''
+def computeSalary(empRatePerHr, working_Hr):
+    return empRatePerHr * working_Hr
+print(f"Employee Salary is Rs {computeSalary(empRatePerHr, workingHrs(empOption))}/-")
