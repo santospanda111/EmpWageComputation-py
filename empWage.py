ac617e153fef05f5d4330e81633ce6ac16f489f0
santospanda111 <santospanda111@gmail.com>
2021-06-15 10:17:35 +0530

    Added UC3 to add part time work

--- empWage.py
+++ empWage.py
@@ -3,13 +3,19 @@ import random
 print("Welcome to Employee Wage Computation Program")
 empRatePerHr = 20
 empHrs = 8
+partTimeHr = 4
+
 '''This condition will check the employee is present or absent.
-   According to the presence of employee it'll calculate the salary. 
+   According to the presence of employee it'll calculate the salary.
+   Added Part time work. 
 '''
-empOption = random.randint(0, 2)
+empOption = random.randint(0, 3)
 if empOption == 1:
     print("Employee is Present")
     print(f"Employee's Salary is Rs {empHrs*empRatePerHr}/-")
+elif empOption == 2:
+    print("Employee is present for Half-Time.")
+    print(f"Employee's Salary is Rs {empRatePerHr*partTimeHr}/-")
 else:
     print("Employee is Absent")
     print("Employee's Salary is Rs 0/-")
