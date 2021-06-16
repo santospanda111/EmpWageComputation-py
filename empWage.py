0e7f6379cff9cc13a71991ba154d16d2c6266623
santospanda111 <santospanda111@gmail.com>
2021-06-16 11:49:00 +0530

    refactored and added UC5 to calculate monthly wage

--- empWage.py
+++ empWage.py
@@ -1,35 +1,48 @@
 import random
+class EmployeeWage:
+    wage_Per_Hour = 20
+    workHour = 0
+    dailyWage = 0
+    isAbsent = 0
+    isFullTime = 1
+    isPartTime = 2
+    fullTimeHours = 8
+    partTimeHours = 4
 
-print("Welcome to Employee Wage Computation Program")
-empRatePerHr = 20
-empHrs = 8
-partTimeHr = 4
+    def checkAttendance(cls):
+        """
+        -This method will get the workHour according to the attendance which will generate random numbers.
+        :return:workHour
+        """
+        attendance = (random.randint(0, 2))
+        if attendance == 1:
+            print('Employee is present full time')
+            EmployeeWage.workHour = 8
+        elif attendance == 2:
+            print('Employee is present part time')
+            EmployeeWage.workHour = 4
+        else:
+            print('Employee is absent')
+            EmployeeWage.workHour = 0
+        return EmployeeWage.workHour
 
-'''
-Added a function which will get the employee hrs.
-according to that it'll print that employee is present or absent. 
-'''
-empOption = random.randint(0, 2)
+    def calculateMonthlyWage(self):
+        """
+        -this one is an instance method where i have called checkAttendance method to get workHours.
+        -calculate monthly wage.
+        :return: monthlyWage
+        """
+        monthlyWage = 0
+        for day in range(1, 21):
+            EmployeeWage.checkAttendance(self)
+            dailyWage = EmployeeWage.wage_Per_Hour * EmployeeWage.workHour
+            print(f"Employee daily Wage is : {dailyWage}")
+            monthlyWage = monthlyWage + dailyWage
+        else:
+            print("Employes's monthly salary", monthlyWage)
 
 
-def workingHrs(empOption):
-    switcher = {
-        0: 0,
-        1: empHrs,
-        2: partTimeHr
-    }
-    working_Hr = switcher.get(empOption)
-    if working_Hr == 8:
-        print("Employee is Present")
-    elif working_Hr == 4:
-        print("Employee is Present for Half-Time")
-    else:
-        print("Employee is Absent")
-    return working_Hr
-
-'''
-this method will calculate the salary and return that.
-'''
-def computeSalary(empRatePerHr, working_Hr):
-    return empRatePerHr * working_Hr
-print(f"Employee Salary is Rs {computeSalary(empRatePerHr, workingHrs(empOption))}/-")
+if __name__ == '__main__':
+    print('Welcome to Employee Wage Computation Program')
+    emp = EmployeeWage()
+    print(emp.calculateMonthlyWage())
